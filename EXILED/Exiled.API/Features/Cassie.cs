e8cfe86b2fa1d92b32f3fcd1ebfd013d0a6d1fd4
TheLongQuite <okilov.rus@gmail.com>
2025-07-16 20:27:15 +0500

    че

--- EXILED/Exiled.API/Features/Cassie.cs
+++ EXILED/Exiled.API/Features/Cassie.cs
@@ -11,14 +11,11 @@ namespace Exiled.API.Features
     using System.Linq;
     using System.Text;
 
+    using Exiled.API.Enums;
     using Exiled.API.Features.Pools;
-
     using MEC;
-
     using PlayerRoles;
-
     using PlayerStatsSystem;
-
     using Respawning;
 
     using CustomFirearmHandler = DamageHandlers.FirearmDamageHandler;
